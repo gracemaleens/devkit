6782775d74312c4ae7efcb1582e6c134d7004bb1
grace <graceens@163.com>
2023-11-02 11:59:10 +0800

    将使用的第三方工具移到当前工程目录下

---
+++ dependencies.py
@@ -0,0 +1,6 @@
+import os
+
+_libs_dir = os.path.join(os.path.dirname(__file__), 'libs')
+
+PEPK = os.path.join(_libs_dir, 'pepk.jar')
+BUNDLETOOL = os.path.join(_libs_dir, 'bundletool-all-1.13.2.jar')
--- devkit.py
+++ devkit.py
@@ -1,6 +1,6 @@
-import os
-
 import click
+
+from dependencies import *
 
 
 @click.group()
@@ -9,29 +9,43 @@ def devkit():
 
 
 @devkit.command('gen-key')
-@click.option('-ks', '--keystore', prompt='密钥库名称', help='密钥库名称')
+@click.option('-ks', '--keystore', prompt='密钥库路径', help='密钥库绝对路径')
 @click.option('-ks-pass', '--ks-pass', prompt='密钥库口令', help='密钥库口令')
 @click.option('-alias', '--alias', prompt='别名', help='别名')
 @click.option('-key-pass', '--key-pass', prompt='密钥口令', help='密钥口令，如果与密钥库口令相同，则不需指定')
 def generate_key(keystore, ks_pass, alias, key_pass):
     """生成签名"""
-    cmd = f'keytool -genkeypair -v -keystore {keystore} -keyalg RSA -keysize 2048 -validity 10000 ' \
-          f'-storepass {ks_pass} -alias {alias}'
+    cmd = f'keytool ' \
+          f'-genkeypair ' \
+          f'-v ' \
+          f'-keystore {keystore} ' \
+          f'-keyalg RSA ' \
+          f'-keysize 2048 ' \
+          f'-validity 10000 ' \
+          f'-storepass {ks_pass} ' \
+          f'-alias {alias}'
     if key_pass is not None:
         cmd += f' -keypass {key_pass}'
     os.system(cmd)
     
     
 @devkit.command('export-pepk')
-@click.option('-ks', '--keystore', prompt='密钥库名称', help='密钥库名称')
+@click.option('-ks', '--keystore', prompt='密钥库路径', help='密钥库绝对路径')
 @click.option('-a', '--alias', prompt='别名', help='别名')
-@click.option('-o', '--output', prompt='输出路径', help='输出路径')
-def export_play_encrypt_private_key(keystore, alias, output):
+@click.option('-o', '--output', default='', help='输出路径')
+@click.option('-epk', '--encryption-public-key-path', prompt='加密公钥', help='从Google Play Console下载的pem格式的加密公钥')
+def export_play_encrypt_private_key(keystore, alias, output, encryption_public_key_path):
+    if output is None or len(output) <= 0:
+        output = f'{os.path.splitext(keystore)[0]}-encrypted.zip'
+
     """导出Google play签名私钥"""
-    cmd = f'%pepk% --keystore={keystore} --alias={alias} --output={output} --include-cert '\
-          f'--encryptionkey'\
-          f'=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb'\
-          f'9d2a529a2092761fb833b656cd48b9de6a'
+    cmd = f'java -jar {PEPK} ' \
+          f'--keystore={keystore} ' \
+          f'--alias={alias} ' \
+          f'--output={output} ' \
+          f'--include-cert '\
+          f'--rsa-aes-encryption ' \
+          f'--encryption-key-path={encryption_public_key_path}'
     os.system(cmd)
     
     
@@ -39,7 +53,7 @@ def export_play_encrypt_private_key(keystore, alias, output):
 @click.option('-apks', '--apks', prompt='apks路径', help='apks路径')
 @click.option('-d', '--device-id', help='如果连接了多个设备，需要指定目标设备id')
 def install_apks(apks, device_id):
-    cmd = f'%bundletool% install-apks --apks={apks}'
+    cmd = f'java -jar {BUNDLETOOL} install-apks --apks={apks}'
     if device_id is not None:
         cmd += f' --device-id {device_id}'
     os.system(cmd)
